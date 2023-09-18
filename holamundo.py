a4d00f969f50bb6b59c94f755f937ccb6671c9ee
ikercalb <ikercaballero271@gmail.com>
2023-09-18 08:59:41 +0200

    holamundo

---
+++ holamundo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import math
+
+v1="holaa"
+v2="mundo"
+array=[1,2,3,4,5]
+print(v1+"  hggfiytd "+v2)
+
+
+if  v1=="hola":
+        print(v1)
+else:
+        print(v2)
+        
+for valor in array:
+        print(valor)
+        
+#te dice que tipo de valor es el que le estas pasando         
+type(v2)
+
+#saca el caracter que pongas  
+texto = "holaa que tal"
+print(texto[0])
+
+
+numero1 = int(input("introduce un numero"))
+numero2 =  int(input("introduce un numero"))
+
+def function_suma(numero1, numero2):
+  numero1
+  numero2
+  
+  suma = (numero1+numero2)
+  
+  return suma
+
+print(function_suma(numero1, numero2))
+
+
